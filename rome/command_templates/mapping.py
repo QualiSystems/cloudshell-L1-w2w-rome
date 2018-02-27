2e76bf574925335cb83c3cb59e93f6363a47007d
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-02-27 16:36:04 +0200

    Fixed templates errors

--- rome/command_templates/mapping.py
+++ rome/command_templates/mapping.py
@@ -3,7 +3,8 @@ from collections import OrderedDict
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
 ACTION_MAP = OrderedDict()
-ERROR_MAP = OrderedDict([(r'[Ee]rror', 'Command error'), (r'ERROR', 'Command error')])
+ERROR_MAP = OrderedDict([(r'[Pp]orts\s+are\s+not\s+of\s+the\s+same\s+matrix', 'Ports are not of the same matrix'),
+                         (r'[Ee]rror', 'Command error'), (r'ERROR', 'Command error')])
 
 CONNECT = CommandTemplate('connection create {src_port} to {dst_port}', ACTION_MAP, ERROR_MAP)
 DISCONNECT = CommandTemplate('connection disconnect {src_port} from {dst_port}', ACTION_MAP, ERROR_MAP)
