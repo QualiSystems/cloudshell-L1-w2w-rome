7b14d9d3925de88a33bc85329fdc08805860d0b9
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-02-27 16:24:26 +0200

    Added  error to the templates

--- rome/command_templates/mapping.py
+++ rome/command_templates/mapping.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
 ACTION_MAP = OrderedDict()
-ERROR_MAP = OrderedDict([(r'[Ee]rror:', 'Command error')])
+ERROR_MAP = OrderedDict([(r'[Ee]rror', 'Command error'), (r'ERROR', 'Command error')])
 
 CONNECT = CommandTemplate('connection create {src_port} to {dst_port}', ACTION_MAP, ERROR_MAP)
 DISCONNECT = CommandTemplate('connection disconnect {src_port} from {dst_port}', ACTION_MAP, ERROR_MAP)
