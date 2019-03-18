11b1e7c8850455164db485cfc0cfc5dc984a2d9a
Kyrylo Maksymenko <saklar13@gmail.com>
2019-03-18 15:29:37 +0200

    send empty line in connecting/disconnecting if doesn't get a prompt

--- rome/command_templates/mapping.py
+++ rome/command_templates/mapping.py
@@ -2,7 +2,10 @@ from collections import OrderedDict
 
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
-ACTION_MAP = OrderedDict()
+ACTION_MAP = OrderedDict([
+    (r'(?i)connection operation',
+     lambda session, logger: session.send_line('', logger)),
+])
 ERROR_MAP = OrderedDict([(r'[Pp]orts\s+are\s+not\s+of\s+the\s+same\s+matrix', 'Ports are not of the same matrix'),
                          (r'[Ee]rror', 'Command error'), (r'ERROR', 'Command error')])
 
