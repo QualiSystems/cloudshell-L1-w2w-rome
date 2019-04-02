aad79ec2e3cdbbc2fad554935282a902101ebd3b
Kyrylo Maksymenko <saklar13@gmail.com>
2019-04-02 13:21:06 +0300

    add regex for connection operations in the command prompt

--- rome/cli/rome_command_modes.py
+++ rome/cli/rome_command_modes.py
@@ -5,8 +5,18 @@ from collections import OrderedDict
 from cloudshell.cli.command_mode import CommandMode
 
 
+CONNECTION_OPERATION_REGEX = (
+    r'(\d{1,2}-\d{1,2}-\d{2,4}\s\d{1,2}:\d{1,2}\s'  # date time
+    r'('
+    r'((dis)?connecting\.{3}\n?)|'
+    r'(connection operation \w+:\w+\[\w+\]<->\w+\[\w+\]\sop:\w+\n?)'
+    r')'
+    r')*'
+)
+
+
 class DefaultCommandMode(CommandMode):
-    PROMPT = r'\w+\[\w+\]#\s*$'
+    PROMPT = r'(?i)\w+\[\w+\]#\s*{}$'.format(CONNECTION_OPERATION_REGEX)
     ENTER_COMMAND = ''
     EXIT_COMMAND = 'exit'
 
