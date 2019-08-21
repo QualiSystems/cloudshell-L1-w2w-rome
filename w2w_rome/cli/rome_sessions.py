36adc20906d06352d4f98bed867f97ad636e524e
Kyrylo Maksymenko <saklar13@gmail.com>
2019-08-21 14:13:07 +0300

    reset pending connections

--- w2w_rome/cli/rome_sessions.py
+++ w2w_rome/cli/rome_sessions.py
@@ -1,9 +1,21 @@
 from collections import OrderedDict
 
+from cloudshell.cli.session.ssh_session import SSHSession
 from cloudshell.cli.session.telnet_session import TelnetSession
 
 
 class RomeTelnetSession(TelnetSession):
+    def __init__(self, host, username, password, *args, **kwargs):
+        super(RomeTelnetSession, self).__init__(
+            host, username, password, *args, **kwargs
+        )
+        self.full_buffer = ''
+
+    def _receive(self, timeout, logger):
+        data = super(RomeTelnetSession, self)._receive(timeout, logger)
+        self.full_buffer += data
+        return data
+
     def _connect_actions(self, prompt, logger):
         action_map = OrderedDict()
         action_map['[Ll]ogin:|[Uu]ser:|[Uu]sername:'] = lambda session, logger: session.send_line(session.username,
@@ -13,3 +25,14 @@ class RomeTelnetSession(TelnetSession):
         self.hardware_expect(None, expected_string=prompt, timeout=self._timeout, logger=logger,
                              action_map=action_map, error_map=error_map)
         self._on_session_start(logger)
+
+
+class RomeSSHSession(SSHSession):
+    def __init__(self, host, username, password, *args, **kwargs):
+        super(RomeSSHSession, self).__init__(host, username, password, *args, **kwargs)
+        self.full_buffer = ''
+
+    def _receive(self, timeout, logger):
+        data = super(RomeSSHSession, self)._receive(timeout, logger)
+        self.full_buffer += data
+        return data
