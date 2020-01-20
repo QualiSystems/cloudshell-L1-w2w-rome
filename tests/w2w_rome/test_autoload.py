d5f2390d7e87eebbc59cb962a88bbf037348ac68
Kyrylo Maksymenko <saklar13@gmail.com>
2020-01-20 21:28:18 +0200

    add test for Q128
    - add ability to work with a few resources in the tests with emulator

--- tests/w2w_rome/test_autoload.py
+++ tests/w2w_rome/test_autoload.py
@@ -6,7 +6,8 @@ from mock import patch, MagicMock
 from w2w_rome.helpers.errors import BaseRomeException
 
 from tests.w2w_rome.base import BaseRomeTestCase, CliEmulator, Command, DEFAULT_PROMPT, \
-    SHOW_BOARD, PORT_SHOW_MATRIX_B, PORT_SHOW_MATRIX_A, PORT_SHOW_MATRIX_Q
+    SHOW_BOARD, PORT_SHOW_MATRIX_B, PORT_SHOW_MATRIX_A, PORT_SHOW_MATRIX_Q, \
+    PORT_SHOW_MATRIX_Q128_1, PORT_SHOW_MATRIX_Q128_2
 
 
 @patch('cloudshell.cli.session.ssh_session.paramiko', MagicMock())
@@ -238,3 +239,85 @@ class RomeTestAutoload(BaseRomeTestCase):
             self.driver_commands.get_resource_description(address)
 
         emu.check_calls()
+
+    def test_autoload_matrix_q128(self):
+        first_host = '192.168.122.10'
+        second_host = '192.168.122.11'
+        address = '{first_host}:{second_host}:q'.format(**locals())
+        user = 'user'
+        password = 'password'
+        expected_ports_str_range = [str(i).zfill(3) for i in range(1, 129)]
+
+        emu1 = CliEmulator(
+            [
+                Command('', DEFAULT_PROMPT),
+                Command(
+                    'port show',
+                    PORT_SHOW_MATRIX_Q128_1,
+                ),
+                Command('show board', SHOW_BOARD),
+            ],
+        )
+        emu2 = CliEmulator(
+            [
+                Command('', DEFAULT_PROMPT),
+                Command(
+                    'port show',
+                    PORT_SHOW_MATRIX_Q128_2,
+                )
+            ]
+        )
+
+        self.send_line_func_map.update(
+            {first_host: emu1.send_line, second_host: emu2.send_line}
+        )
+        self.receive_all_func_map.update(
+            {first_host: emu1.receive_all, second_host: emu2.receive_all}
+        )
+        with self.patch_sessions():
+            self.driver_commands.login(address, user, password)
+
+        info = self.driver_commands.get_resource_description(address)
+
+        self.assertIsNotNone(info)
+        self.assertEqual(1, len(info.resource_info_list))
+
+        chassis = info.resource_info_list[0]
+        self.assertIsInstance(chassis, Chassis)
+        self.assertEqual('9727-4733-2222', chassis.serial_number)
+        self.assertEqual('Rome Chassis', chassis.model_name)
+        self.assertEqual(1, len(chassis.child_resources))
+
+        blade = chassis.child_resources.values()[0]
+        self.assertIsInstance(blade, Blade)
+        self.assertEqual('Rome Matrix Q', blade.model_name)
+        self.assertEqual('NA', blade.serial_number)
+        self.assertEqual('Blade Q', blade.name)
+        self.assertEqual(128, len(blade.child_resources))
+        self.assertItemsEqual(
+            expected_ports_str_range,
+            blade.child_resources.keys(),
+        )
+        self.assertItemsEqual(
+            map('Port Q{}'.format, expected_ports_str_range),
+            (port.name for port in blade.child_resources.values()),
+        )
+
+        connected_ports = []
+        for port_id, port in blade.child_resources.items():
+            self.assertIsInstance(port, Port)
+            self.assertEqual('Port Q{}'.format(port.resource_id), port.name)
+            if port.mapping:
+                connected_ports.append(port)
+
+        self.assertEqual(2, len(connected_ports))
+        self.assertDictEqual(
+            {
+                'Port Q001': 'Port Q002',
+                'Port Q002': 'Port Q001',
+            },
+            {p.name: p.mapping.name for p in connected_ports},
+        )
+
+        emu1.check_calls()
+        emu2.check_calls()
